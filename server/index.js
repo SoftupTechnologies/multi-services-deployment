5e83e403107357952f4d7cb128320c9478018d75
Kristi 😎 <kristi.kristo@wirecard.com>
2019-07-02 14:26:22 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'healthy'
+  });
+});
+
+app.listen(5000, err => {
+  console.log('Listening');
+});
